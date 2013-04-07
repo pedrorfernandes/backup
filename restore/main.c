1f7ab250b79698f37e8a7bfb6c78f5a7dbbd26f0
Nanner <nanner@Clara.(none)>
2013-04-08 00:39:26 +0100

    Added a a string array creating function. Modified the printFolders function. Removed unused structs and stuff. Modified DATE_LEN to 19 instead of 20. Created getNumOfDirectories that returns, well, the number of directories. Created getLineAt, that returns the line in the given position of the given file. Modified getBackupInfo that now only returns a string with path/backupInfo. Created getBackupFullPath that returns the full path to the given backup directory. Created printFiles that (supposedly, not quite tested yet) prints the files in a directory. Made some self explanatory changes on main.

--- restore/main.c
+++ restore/main.c
@@ -25,107 +25,221 @@
 #include <fcntl.h>
 
 #define PROMPT "> "
-#define DATE_LEN 20
+#define DATE_LEN 19
 #define MAX_LEN 1024
 #define BACKUP_INFO_LEN 33
 #define BACKUPINFO "__bckpinfo__"
 
-struct BackupInfo {
-    char date[DATE_LEN];
-    char filename[MAX_LEN];
-    char path[MAX_LEN];
-};
+char** createStrArray(unsigned int numberOfStrings, unsigned int sizeOfStrings) {
+  
+    char ** strArray = malloc ( numberOfStrings * sizeof ( char* ) );
+    
+    int i;
+    
+    for ( i = 0 ; i < numberOfStrings; ++i ) {
+        strArray[i] = malloc ( sizeOfStrings * sizeof ( char ) );
+    }
+    
+    return strArray;
+    
+}
+  
 
-int getNumOfLines(const char * filePath){
+int getNumOfLines ( const char * filePath )
+{
     int file;
-    file = open(filePath, O_RDONLY);
-    if (file == -1){
-        perror(filePath);
-        exit(5);
+    file = open ( filePath, O_RDONLY );
+    if ( file == -1 ) {
+        perror ( filePath );
+        exit ( 5 );
     }
     unsigned char ch[1];
     int number_of_lines = 1;
 
-	while ( read(file, ch, 1) > 0){
-        if( ch[0] == '\n') number_of_lines++;
+    while ( read ( file, ch, 1 ) > 0 ) {
+        if ( ch[0] == '\n' ) {
+            number_of_lines++;
+        }
     }
-    
+
     return number_of_lines;
 }
 
-int* getBackupInfo(const char * restoreDate){
-    DIR *backupFolder;
-    if ( (backupFolder = opendir(restoreDate) ) == NULL) {
-        perror(restoreDate);
-        exit(4);
+int getNumOfDirectories ( DIR * folderDir )
+{
+    struct dirent *direntp;
+    struct stat stat_buf;
+    int n = 0;
+
+    while ( ( direntp = readdir ( folderDir ) ) != NULL ) {
+        if ( stat ( direntp->d_name, &stat_buf ) != 0 ) {
+            printf ( "Error number %d: %s\n", errno, strerror ( errno ) );
+            return -1;
+        }
+        if ( S_ISDIR ( stat_buf.st_mode )
+                // and ignore the "." and ".."
+                && strcmp ( direntp->d_name, "." )
+                && strcmp ( direntp->d_name, ".." ) ) {
+            n++;
+        }
     }
-    
-    char backupInfoPath[BACKUP_INFO_LEN];
-    sprintf(backupInfoPath, "%s/%s", restoreDate, BACKUPINFO);
-    struct BackupInfo files [ getNumOfLines(backupInfoPath) ];
-    
-    
-    
+
+    rewinddir ( folderDir );
+    return n;
 }
 
-int printFolders(DIR * backupDir){
+
+char* getLineAt ( unsigned int line, const char * filePath )
+{
+
+    if ( line <= 0 || line > getNumOfLines ( filePath ) ) {
+        printf ( "Tried to access unexistent line at %s\n", filePath );
+        exit ( -1 );
+    }
+
+    FILE * file;
+    file = fopen ( filePath, "r" );
+
+    if ( file == NULL ) {
+        perror ( filePath );
+        exit ( 5 );
+    }
+
+    char str[MAX_LEN];
+
+    int n = 0;
+    while ( n != line ) {
+        n++;
+        fgets ( str, MAX_LEN, file );
+    }
+
+    return str;
+}
+
+char* getBackupInfo ( const char* restoreDate ) {
+    char backupInfoPath[BACKUP_INFO_LEN];
+    sprintf ( backupInfoPath, "%s/%s", restoreDate, BACKUPINFO );
+    return backupInfoPath;
+}
+
+char* getBackupFullPath (const char* path, const char* backupPath ) {
+  
+    char backupFullPath[MAX_LEN];
+    sprintf ( backupFullPath, "%s/%s", path, backupPath );
+    return backupFullPath;
+}
+
+
+char** getAndPrintFolders ( DIR * backupDir )
+{
     struct dirent *direntp;
     struct stat stat_buf;
 
-    while ( (direntp = readdir(backupDir)) != NULL)
-    {
-        if ( stat(direntp->d_name, &stat_buf) != 0 ){
-            printf("Error number %d: %s\n", errno, strerror(errno));
-            return -1;
-        }
-        if (S_ISDIR(stat_buf.st_mode)
-            // and ignore the "." and ".."
-            && strcmp(direntp->d_name, ".")
-            && strcmp(direntp->d_name, "..") )
-            printf("%-25s\n", direntp->d_name);
-    }
+    int numberOfBackups = getNumOfDirectories ( backupDir );
     
-    return 0;
+    char** backups = createStrArray(numberOfBackups, DATE_LEN);
+
+    int n = 0;
+
+    while ( ( direntp = readdir ( backupDir ) ) != NULL ) {
+        if ( stat ( direntp->d_name, &stat_buf ) != 0 ) {
+            printf ( "Error number %d: %s\n", errno, strerror ( errno ) );
+            return NULL;
+        }
+        if ( S_ISDIR ( stat_buf.st_mode )
+                // and ignore the "." and ".."
+                && strcmp ( direntp->d_name, "." )
+                && strcmp ( direntp->d_name, ".." ) ) {
+            sprintf (backups[n], "%s", direntp->d_name );
+            printf ( "%d- %-25s\n", n, direntp->d_name );
+            n++;
+        }
+    }
+
+    rewinddir ( backupDir );
+    return backups;
 }
 
-int main(int argc, const char * argv[])
+int printFiles (DIR * backupDir) {
+    struct dirent *direntp;
+    struct stat stat_buf;
+
+    int n = 1;
+
+    while ( ( direntp = readdir ( backupDir ) ) != NULL ) {
+        if ( stat ( direntp->d_name, &stat_buf ) != 0 ) {
+            printf ( "Error number %d: %s\n", errno, strerror ( errno ) );
+            return -1;
+        }
+        if ( S_ISREG ( stat_buf.st_mode )
+                // and ignore the "." and ".."
+                && strcmp ( direntp->d_name, "." )
+                && strcmp ( direntp->d_name, ".." ) ) {
+            printf ( "%d- %-25s\n", n, direntp->d_name );
+            n++;
+        }
+    }
+
+    rewinddir ( backupDir );
+    return 0;
+}
+    
+  
+
+int main ( int argc, const char * argv[] )
 {
 
     // usage: rstr dir2 dir3
-    if (argc != 3){
-        fprintf( stderr, "Usage: %s dir_backup dir_restore\n", argv[0]);
-        exit(1);
+    if ( argc != 3 ) {
+        fprintf ( stderr, "Usage: %s dir_backup dir_restore\n", argv[0] );
+        exit ( 1 );
     }
-    
+
     DIR *backupDir;
     DIR *restoreDir;
 
-    if ( (backupDir = opendir( argv[1]) ) == NULL) {
-        perror(argv[1]);
-        exit(2);
+    if ( ( backupDir = opendir ( argv[1] ) ) == NULL ) {
+        perror ( argv[1] );
+        exit ( 2 );
     }
-    
-    if ( (restoreDir = opendir( argv[2]) ) == NULL) {
+
+    if ( ( restoreDir = opendir ( argv[2] ) ) == NULL ) {
         // if restore dir doesnt exist, create it
-        mkdir(argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        if ( (restoreDir = opendir( argv[2]) ) == NULL) {
-            perror(argv[1]);
-            exit(3);
+        mkdir ( argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH );
+        if ( ( restoreDir = opendir ( argv[2] ) ) == NULL ) {
+            perror ( argv[1] );
+            exit ( 3 );
         }
     }
-    
-    printf("The following restore points are available:\n");
-    printf("(year_month_day_hours_minutes_seconds)\n");
-    chdir(argv[1]); // this avoids any errors with stat()
-    printFolders(backupDir);
 
-    printf("Which restore point?\n%s", PROMPT);
-    char restoreDate[DATE_LEN];
-    scanf("%s", restoreDate);
+    printf ( "The following restore points are available:\n" );
+    printf ( "(year_month_day_hours_minutes_seconds)\n" );
+    chdir ( argv[1] ); // this avoids any errors with stat()
+
+    char** backups = getAndPrintFolders ( backupDir );
+
+    printf ( "Which restore point?\n%s", PROMPT );
+
+    char lineSelection = getchar();
+    int lineNumber = lineSelection - '0';
     
-    getBackupInfo(restoreDate);
+    char* selectedBckpPath = getBackupFullPath(argv[1], backups[lineNumber]);
     
+    //Super testing printf
+    printf("Selected backup path:\n\n%s\n", selectedBckpPath);
     
+    DIR *selectedBackup;
+
+    if ( ( selectedBackup = opendir ( selectedBckpPath ) ) == NULL ) {
+        perror ( selectedBckpPath );
+        exit ( 2 );
+    }
+    
+    printf ( "This backup contains the following files:\n" );
+    printFiles(selectedBackup);
+
+   // printf ( "%s\n", getLineAt ( lineNumber, getBackupInfo ( backups[lineNumber] ) ) );
+
     return 0;
 }
 
