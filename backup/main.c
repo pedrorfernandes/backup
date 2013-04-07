b1119eead874c4a49e3a7c25c6c3b317d405cc08
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-04-07 16:20:22 +0100

    initial commit, added xcode projects for backup and restore

---
+++ backup/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  backup
+//
+//  Created by pedro on 4/7/13.
+//  Copyright (c) 2013 feup. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+    // bla bla
+}
+
---
+++ restore/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  restore
+//
+//  Created by pedro on 4/7/13.
+//  Copyright (c) 2013 feup. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
