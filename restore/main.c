6549afccd7fed3920152bc6b4057e06fec3f2319
pedrorfernandes <pedrorfernandes@yahoo.com>
2013-04-07 18:35:51 +0100

    print the available restore points to choose

--- restore/main.c
+++ restore/main.c
@@ -15,11 +15,61 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROMPT "> "
+
+int printContents(DIR * backupDir){
+    struct dirent *direntp;
+    struct stat stat_buf;
+
+    while ( (direntp = readdir(backupDir)) != NULL)
+    {
+        if ( stat(direntp->d_name, &stat_buf) != 0 ){
+            printf("Error number %d: %s\n", errno, strerror(errno));
+            return -1;
+        }
+        if (S_ISDIR(stat_buf.st_mode)
+            && strcmp(direntp->d_name, ".") && strcmp(direntp->d_name, "..") )
+            printf("%-25s\n", direntp->d_name);
+    }
+    
+    return 0;
+}
 
 int main(int argc, const char * argv[])
 {
 
-    // check arguments
+    // usage: rstr dir1 dir2
+    if (argc != 3){
+        fprintf( stderr, "Usage: %s dir_with_files dir_backup\n", argv[0]);
+        exit(1);
+    }
+    
+    DIR *filesDir;
+    DIR *backupDir;
+
+    if ( (filesDir = opendir( argv[1]) ) == NULL) {
+        perror(argv[1]);
+        exit(2);
+    }
+    
+    if ( (backupDir = opendir( argv[2]) ) == NULL) {
+        perror(argv[2]);
+        exit(3);
+    }
+    
+    printf("The following restore points are available:\n");
+    printf("(year_month_day_hours_minutes_seconds)\n");
+    chdir(argv[2]); // this avoids any errors with stat()
+    printContents(backupDir);
+    printf("Which restore point?\n%s", PROMPT);
+    getchar();
     
     return 0;
 }
